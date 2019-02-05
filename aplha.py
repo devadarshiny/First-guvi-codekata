94667c6586835a491263dc36aa24bab060cf5540
devadarshiny <47316145+devadarshiny@users.noreply.github.com>
2019-02-05 12:24:55 +0530

    Update aplha.py

--- aplha.py
+++ aplha.py
@@ -1,8 +1,8 @@
-ch = input("Input ");
+ch = input();
 if ch == '0':
     exit();
 else:
     if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
-    	print(ch, " Alphabet.");
+    	print("Alphabet.");
     else:
     	print( "No.");
